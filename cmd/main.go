049b1e09f1871b1a4ad6b09f81fe808fe0899cc4
Malik <malikoabdulaziz@gmail.com>
2023-05-22 21:02:58 +0100

    add exit codes on err

--- cmd/main.go
+++ cmd/main.go
@@ -10,17 +10,18 @@ import (
 func main() {
 	if len(os.Args) == 1 {
 		fmt.Println("we need args bro pass -h for help")
-		return
+		os.Exit(1)
 	}
 	if strings.ToLower(os.Args[1]) == "-v" {
 		fmt.Println("v 0.0.1")
-		return
+		os.Exit(0)
+
 	}
 	if strings.ToLower(os.Args[1]) == "-h" {
 		fmt.Println("welcome!")
 		fmt.Println("pass me a file or files as args and i'll tell you some stuff about it.")
-		return
+		os.Exit(1)
 	}
 	file.File(os.Args[1:])
-
+	os.Exit(0)
 }
